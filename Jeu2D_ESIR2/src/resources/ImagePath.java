ecbf20ee00345c68f6f09be31caa6f0a864fb10b
trist <trist@10.41.7.203>
2023-05-16 17:33:19 +0200

    Mise a jour image path + image .png

--- Jeu2D_ESIR2/src/resources/ImagePath.java
+++ Jeu2D_ESIR2/src/resources/ImagePath.java
@@ -8,21 +8,43 @@ import javax.imageio.ImageIO;
 public class ImagePath {
 	private static ImagePath imgStore;
 
-	public BufferedImage WALL;
-	public BufferedImage GRASS;
-	public BufferedImage PLAYER;
-	//TODO
-	public BufferedImage BAT;
-	public BufferedImage BUTLER;
 	public BufferedImage BARRIER;
+	public BufferedImage BAT;
+	public BufferedImage BATH;
+	public BufferedImage BED;
+	public BufferedImage BUTLER;
+	public BufferedImage COBWEB;
+	public BufferedImage CLOSECHEST;
+	public BufferedImage GRASS;
 	public BufferedImage KEY;
+	public BufferedImage MAINDOOR;
+	public BufferedImage OPENCHEST;
+	public BufferedImage PLAYER;
+	public BufferedImage SWORD;
+	public BufferedImage WALL;
+	public BufferedImage WOODENFLOOR;
+	
+	
 
 
 	private ImagePath() {
 		try {
-			WALL = ImageIO.read(getClass().getResource("/tiles/BRICK2.png"));
+			BARRIER = ImageIO.read(getClass().getResource("/tiles/BARRIER.png"));
+			BAT = ImageIO.read(getClass().getResource("/tiles/TheBat.png"));
+			BATH = ImageIO.read(getClass().getResource("/tiles/BATH.png"));
+			BED = ImageIO.read(getClass().getResource("/tiles/BED.png"));
+			BUTLER = ImageIO.read(getClass().getResource("/tiles/SKELETON.png"));
+			CLOSECHEST = ImageIO.read(getClass().getResource("/tiles/CLOSECHEST.png"));
+			COBWEB = ImageIO.read(getClass().getResource("/tiles/COBWEB.png"));
 			GRASS = ImageIO.read(getClass().getResource("/tiles/GRASS.png"));
+			KEY = ImageIO.read(getClass().getResource("/tiles/KEY.png"));
+			MAINDOOR = ImageIO.read(getClass().getResource("/tiles/castledoors.png"));
+			OPENCHEST = ImageIO.read(getClass().getResource("/tiles/OPENCHEST.png"));
 			PLAYER = ImageIO.read(getClass().getResource("/Player/superhero.png"));
+			SWORD = ImageIO.read(getClass().getResource("/tiles/SWORD.png"));
+			WOODENFLOOR = ImageIO.read(getClass().getResource("/tiles/woodenfloor.png"));
+			WALL = ImageIO.read(getClass().getResource("/tiles/BRICK2.png"));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
