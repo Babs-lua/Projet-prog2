8d373ef7b98b9fcd9b7c1d911fd6d1774d1a5565
Bastien Dmrx <bastien.demorieux@orange.fr>
2023-05-16 13:45:19 +0200

    Fichier reprenant les répertoires d'images

---
+++ Jeu2D_ESIR2/src/resources/ImagePath.java
@@ -0,0 +1,12 @@
+package resources;
+
+public class ImagePath {
+	public static final String WALL = "/tile/BRICK.png";
+	public static final String BARRIER = "";
+	public static final String GRASS = "/tile/GRASS.png";
+	public static final String BUTLER = "";
+	public static final String BAT = "";
+	public static final String PLAYER = "/Player/superhero.png";
+	public static final String LAVA="/tiles/LAVA.png";
+	
+}
