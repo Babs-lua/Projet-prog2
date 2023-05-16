4c0e85627d26ff451b4277fb0941db80c548e7d6
Bastien Dmrx <bastien.demorieux@orange.fr>
2023-05-16 14:37:01 +0200

    Changement du répertoire des images. Pour affecter une image à un objet utilisé : " ImagePath.getInstace().nomDeLImage"

--- Jeu2D_ESIR2/src/resources/ImagePath.java
+++ Jeu2D_ESIR2/src/resources/ImagePath.java
@@ -1,12 +1,42 @@
 package resources;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class ImagePath {
-	public static final String WALL = "/tile/BRICK.png";
-	public static final String BARRIER = "";
-	public static final String GRASS = "/tile/GRASS.png";
-	public static final String BUTLER = "";
-	public static final String BAT = "";
-	public static final String PLAYER = "/Player/superhero.png";
-	public static final String LAVA="/tiles/LAVA.png";
-	
+	private static ImagePath imgStore;
+
+	private String wall = "/tile/BRICK.png";
+	private String barrier = "";
+	private String grass = "/tile/GRASS.png";
+	private String butler = "";
+	private String bat = "";
+	private String player = "/Player/superhero.png";
+	private String lava = "/tiles/LAVA.png";
+
+	public BufferedImage WALL;
+	public BufferedImage BARRIER;
+	public BufferedImage PLAYER;
+
+	private ImagePath() {
+		try {
+			WALL = ImageIO.read(getClass().getResource(wall));
+			BARRIER= ImageIO.read(getClass().getResource(barrier));
+			PLAYER=ImageIO.read(getClass().getResource(player));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void createSingleton() {
+		if (imgStore == null)
+			imgStore = new ImagePath();
+	}
+
+	public static ImagePath getInstance() {
+		return imgStore;
+	}
+
 }
