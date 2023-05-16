928d441a9f4a4ee332699327cf5d1ab08ec0998e
Bastien Dmrx <bastien.demorieux@orange.fr>
2023-05-16 17:39:43 +0200

    Modification de la sword

--- Jeu2D_ESIR2/src/entity/pickableObjects/Sword.java
+++ Jeu2D_ESIR2/src/entity/pickableObjects/Sword.java
@@ -1,14 +1,16 @@
 package entity.pickableObjects;
 
+import entity.Objet;
+import main.GamePanel;
 import resources.ImagePath;
 
-public class Sword extends Object {
+public class Sword extends Objet {
 
 	
 	private int effect;
 
-	public Sword(int x, int y) {
-		super(x,y,ImagePath.getInstance().SWORD);
+	public Sword(int x, int y, GamePanel gp) {
+		super(x,y,ImagePath.getInstance().SWORD, gp);
 	}
 	
 	
