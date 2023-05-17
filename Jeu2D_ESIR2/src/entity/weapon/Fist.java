978b3e4eccc7ef402c9c635102340dfed61664dd
Bastien Dmrx <bastien.demorieux@orange.fr>
2023-05-17 12:17:31 +0200

    Création de portée variable en fonction de l'arme

--- Jeu2D_ESIR2/src/entity/weapon/Fist.java
+++ Jeu2D_ESIR2/src/entity/weapon/Fist.java
@@ -1,14 +1,17 @@
 package entity.weapon;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import entity.Player;
 import main.GamePanel;
+import resources.FixedValues;
 
 public class Fist extends Weapon {
 
 	public Fist(int a_x, int a_y, GamePanel g_p) {
 		super(a_x, a_y, null, g_p);
+		this.m_hitbox = new Rectangle(m_x, m_y, FixedValues.fist_size, FixedValues.fist_size);
 	}
 
 	@Override
--- Jeu2D_ESIR2/src/resources/FixedValues.java
+++ Jeu2D_ESIR2/src/resources/FixedValues.java
@@ -8,6 +8,7 @@ public class FixedValues {
 	public static final int box_length = 4;
 	public static final int box_width = 2;
 	public static final int TILE = 48;
-	public static final int sword_length = 48;
+	public static final int sword_length = 80;
 	public static final int sword_width = 48;
+	public static final int fist_size = 48;
 }
