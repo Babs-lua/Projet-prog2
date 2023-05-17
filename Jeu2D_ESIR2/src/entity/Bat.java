824b14943c26c82b167bf71e90b43554b90deffb
Bastien Dmrx <bastien.demorieux@orange.fr>
2023-05-17 02:53:04 +0200

    Nouveau système de déplacement des Bats

--- Jeu2D_ESIR2/src/entity/Bat.java
+++ Jeu2D_ESIR2/src/entity/Bat.java
@@ -1,34 +1,67 @@
 package entity;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.HashSet;
-import javax.imageio.ImageIO;
-import controls.Controls;
-import entity.Monster;
-import entity.Player;
+import java.awt.Point;
+import java.util.HashMap;
+
 import main.GamePanel;
 import resources.ImagePath;
 
 public class Bat extends Monster {
 
-	public Bat(GamePanel a_gp) {
-		super(50, 50, 3, ImagePath.getInstance().BAT, a_gp, 3, 1,30);
+	private enum Parameters {
+		distance, XDirection, YDirection
+	}
+
+	private final int slideValue = 96;
+	private int b_gameCycle;
+	private HashMap<Parameters, Double> attackParameters;
+
+	public Bat(int x, int y, GamePanel a_gp) {
+		super(50, 50, 3, ImagePath.getInstance().BAT, a_gp, 3, 1, 30);
+		b_gameCycle = 0;
+		attackParameters = new HashMap<>();
+	}
+
+	public void aim(Player p) {
+		int deplacement_x = p.m_x - m_x;
+		int deplacement_y = p.m_y - m_y;
+		double distance = Math.sqrt(deplacement_x * deplacement_x + deplacement_y * deplacement_y);
+		attackParameters.put(Parameters.distance, distance);
+		attackParameters.put(Parameters.XDirection, (deplacement_x / distance) * m_speed);
+		attackParameters.put(Parameters.YDirection, (deplacement_y / distance) * m_speed);
+	}
+
+	public void go() {
+		Point newPosition = new Point();
+		newPosition.x = (int) (m_x + attackParameters.get(Parameters.XDirection) +  (slideValue * attackParameters.get(Parameters.XDirection) < 0 ? -1 : 1));
+		newPosition.y = (int) (m_y + attackParameters.get(Parameters.YDirection) + (slideValue * attackParameters.get(Parameters.YDirection) < 0 ? -1 : 1));
+
+		if (!isCollision(newPosition.x, newPosition.y)) {
+			if (newPosition.x != m_gp.getM_Player().m_x || newPosition.y != m_gp.getM_Player().m_y) {
+				m_x += attackParameters.get(Parameters.XDirection)
+						+ (slideValue * attackParameters.get(Parameters.XDirection) < 0 ? -1 : 1);
+				m_y += attackParameters.get(Parameters.YDirection)
+						+ (slideValue * attackParameters.get(Parameters.YDirection) < 0 ? -1 : 1);
+			} else  {
+				m_x += attackParameters.get(Parameters.XDirection);
+				m_y += attackParameters.get(Parameters.YDirection);
+			}
+		}
 	}
 
 	@Override
 	public void move() {
-		int p_x = m_gp.getM_Player().m_x;
-		int p_y = m_gp.getM_Player().m_y;
+		if (!isCollisionWithEnt(m_gp.getM_Player())) {
+			if (b_gameCycle % 120 < 30) {
+				aim(m_gp.getM_Player());
+				go();
+			} else if (b_gameCycle % 180 < 100) {
+				go();
+			} else {
 
-		int deplacement_x = m_gp.getM_Player().m_x - m_x;
-		int deplacement_y = m_gp.getM_Player().m_y - m_y;
+			}
+			b_gameCycle++;
+		}
 
-		double distance = Math.sqrt(deplacement_x * deplacement_x + deplacement_y * deplacement_y);
-		double deplacementNormaliseX = (deplacement_x / distance) * m_speed;
-		double deplacementNormaliseY = (deplacement_y / distance) * m_speed;
-
-		m_x += deplacementNormaliseX;
-		m_y += deplacementNormaliseY;
 	}
 }
